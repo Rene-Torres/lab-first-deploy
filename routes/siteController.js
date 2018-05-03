5ae731c96cac4f3798ebc9c32eb32f716252fc00
Rene-Torres <torress.alvarez@gmail.com>
2018-05-03 11:32:39 -0500

    mail

--- routes/siteController.js
+++ routes/siteController.js
@@ -1,5 +1,36 @@
 const express = require("express");
 const siteController = express.Router();
+const nodemailer=require('nodemailer');
+
+siteController.get('/send-mail', (req, res)=>{
+  res.render('site/mailer')
+})
+
+
+
+siteController.post('/send-email', (req, res, next) => {
+  const tp = nodemailer.createTransport({
+    service :"Gmail",
+    auth:{
+      user:process.env.EMAIL_USER,
+      pass:proccess.env.EMAIL_PASSWORD
+    }
+  });
+
+  tp.sendMail({
+    from: '"My Awesome Bliss 👻" <renixo02@gmail.com>',
+    to: email,
+    subject: Subject,
+    text: "Awesome Message",
+    html: `<b>${message}</b>`
+})
+//.then(info => console.log(info))
+//.catch(error => console.log(error))
+res.send('Correo enviado');
+  //let { email, subject, message } = req.body;
+  //res.render('message', { email, subject, message })
+});
+
 
 siteController.get("/", (req, res, next) => {
   res.render("site/index", { apiKey: "AIzaSyDmsqiHyfwW5OBOax6qlJ2419HLTz29Cec" });
